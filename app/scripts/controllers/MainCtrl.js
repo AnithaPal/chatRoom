7d40306a4c84a8f77014e9c9fc1fdf0aa0919132
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-16 11:06:15 -0500

    main controller modified according to ui model

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,17 +1,24 @@
 (function(){
-  function MainCtrl(RoomService){
+  function MainCtrl($uibModal, RoomService){
     var ctrl = this;
 
     ctrl.rooms = RoomService.bind();
 
-    ctrl.addRoom = function(room){
-      RoomService.addRoom(room);
-    }
-
+    ctrl.open = function () {
+        $uibModal.open({
+          templateUrl: '/templates/modal_instance.html',
+          controller: 'ModalInstanceCtrl',
+          resolve: {
+            rooms: function () {
+              return ctrl.rooms;
+            }
+          }
+        });
+      };
   };
 
   angular
-    .module("chatRoom")
-    .controller("MainCtrl", ['RoomService', MainCtrl]);
+    .module('chatRoom')
+    .controller('MainCtrl', ['$uibModal', 'RoomService', MainCtrl]);
 
 })();
