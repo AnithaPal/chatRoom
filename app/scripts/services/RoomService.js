de9d7bbe0a403cf9849138837c42514208146b28
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-16 13:59:26 -0500

    method added to display messages with the equalent roomId

--- app/scripts/services/RoomService.js
+++ app/scripts/services/RoomService.js
@@ -1,13 +1,23 @@
 (function(){
   function RoomService($firebaseArray){
     var RoomService = {}
+
     var firebaseRef = new Firebase("https://chatroom-letsdiscuss.firebaseio.com/");
-    var rooms = $firebaseArray(firebaseRef.child('rooms'));
+    var roomRef = new Firebase("https://chatroom-letsdiscuss.firebaseio.com/rooms");
+    var messageRef = new Firebase("https://chatroom-letsdiscuss.firebaseio.com/messages");
+    var rooms = $firebaseArray(roomRef);
+    var messages = $firebaseArray(messageRef);
 
     RoomService.addRoom = function(room){
       rooms.$add({name: room.name,
                   created_at: Firebase.ServerValue.TIMESTAMP})
-    }
+    };
+
+    RoomService.getMessages = function(roomId, callback){
+      messageRef.orderByChild('roomId').equalTo(roomId).on('value', function(messages) {
+        callback(messages.val());
+        });
+    };
 
     RoomService.bind = function() {
       return rooms;
