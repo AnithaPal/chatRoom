9a3a8e8f0c19d212d8ddd4bd56fc5ab6f0d30737
AnithaPal <anitha.aravindvel@gmail.com>
2016-02-16 14:00:05 -0500

    message service added to the main controller and message display feature added

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,5 +1,5 @@
 (function(){
-  function MainCtrl($uibModal, RoomService){
+  function MainCtrl($uibModal, RoomService, MessageService, $scope){
     var ctrl = this;
 
     ctrl.rooms = RoomService.bind();
@@ -15,10 +15,24 @@
           }
         });
       };
+
+    ctrl.setRoom = function(room){
+      $scope.roomTitle = room.name;
+      RoomService.getMessages(room.$id, function (messages) {
+        $scope.messages = messages;
+        // $scope.roomId = room.$id;
+      });
+
+    };
+
+    ctrl.createMessage = function(){
+      MessageService.addRoom(ctrl.message);
+    }
+
   };
 
   angular
     .module('chatRoom')
-    .controller('MainCtrl', ['$uibModal', 'RoomService', MainCtrl]);
+    .controller('MainCtrl', ['$uibModal', 'RoomService','MessageService', '$scope', MainCtrl]);
 
 })();
